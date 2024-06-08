2a754966f6ad128687d96a0ddda739de7b4f3484
cathlauretta <18221157@std.stei.itb.ac.id>
2024-06-08 19:19:28 +0700

    feat: add async func

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,65 @@
+'use client'
 import Image from "next/image";
 import Navbar from "../components/navbar";
 import LogCard from "@/components/card";
+import { useEffect, useState } from "react";
 
-// async function getData() {
-//   const res = await fetch('https://simanis.stei.itb.ac.id/aan/api/')
-//   // The return value is *not* serialized
-//   // You can return Date, Map, Set, etc.
+async function login() {
+  const res = await fetch('https://simanis.stei.itb.ac.id/aan/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({"device_id":  "A1", 
+    "password": "Aan"}),
+  }).then(res => console.log(res)).catch(err => console.error(err))
  
-//   if (!res.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error('Failed to fetch data')
-//   }
+  if (!res.ok) {
+    throw new Error('Failed to login')
+  }
  
-//   return res.json()
-// }
+  const { token } = await res.json()
+
+  localStorage.setItem('token', token);
+
+  console.log(token)
+
+  return token
+}
+
+async function getData() {
+  const res = await fetch('https://simanis.stei.itb.ac.id/aan/api/data',
+    {
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiIiwiZGV2aWNlX2lkIjoiQTEiLCJpYXQiOjE3MTU2NzExNzl9.csyGnh60ewFfDhf1MgD8Tgx7RhnpjjzaOyxWL_G2tmY`
+      }
+    }
+  ).then(res => console.log(res)).catch(err => console.error(err))
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
 
 export default async function Home() {
-  // const data = await getData()
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    login()
+    getData()
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  })
+
   return (
     <main className="flex min-h-screen flex-col items-center gap-16">
       <Navbar />
